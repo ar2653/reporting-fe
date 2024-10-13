52dc58fa7337e041fe47e7538bb00f6e8d8031f1
Ankush Ranapure <ar2653@njit.edu>
2024-10-13 15:00:56 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <a className="btn btn-ghost text-xl">Reporting Tool</a>
+    </div>
+  )
+}
+
+export default Navbar;
